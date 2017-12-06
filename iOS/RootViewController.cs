e80afc58aeb6fa8b0b03e7464a3d63e27707d1ec
Alex Chernov <alexusblack@gmail.com>
2017-12-06 14:21:27 +1100

    reorganising and commenting code for iOS native side

--- iOS/RootViewController.cs
+++ iOS/RootViewController.cs
@@ -37,12 +37,80 @@ namespace Moduware.Tile.Speaker.iOS
             // We need to know when core is ready so we can start listening for data from gateways
             CoreReady += CoreReadyHandler;
             // And we need to know when we are ready to send commands
-            ConfigurationApplied += CoreConfigurationApplied;
+            ConfigurationApplied += CoreConfigurationAppliedHandler;
 
             LoadButtonImages();
             base.ViewDidLoad(); 
         }
 
+#region CoreEventHandlers
+
+        /// <summary>
+        /// When core is ready initialising our shared code
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="_e"></param>
+        private void CoreReadyHandler(Object source, EventArgs _e)
+        {
+            _speaker = new SpeakerTile(Core, this);
+        }
+
+        /// <summary>
+        /// When we recieved configuration from main app, requesting speaker status
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CoreConfigurationAppliedHandler(object sender, EventArgs e)
+        {
+            _speaker.RequestStatus();
+        }
+
+        #endregion
+
+#region UiEventHandlers
+
+        /// <summary>
+        /// When main speaker button clicked we need send command to speaker module
+        /// </summary>
+        /// <param name="sender"></param>
+        partial void SpeakerButtonDown(UIKit.UIButton sender)
+        {
+            SetSpeakerButtonState(!_active);
+            if (_active)
+            {
+                _speaker.TurnOn();
+            }
+            else
+            {
+                _speaker.TurnOff();
+            }
+        }
+
+        /// <summary>
+        /// When back button pressed we need open dashboard
+        /// </summary>
+        /// <param name="sender"></param>
+        partial void BackButtonDown(UIKit.UIButton sender)
+        {
+            Utilities.OpenDashboard();
+        }
+
+        /// <summary>
+        /// When default state switch change commanding speaker to change config
+        /// </summary>
+        /// <param name="sender"></param>
+        partial void DefaultStateButtonChange(UISwitch sender)
+        {
+            _speaker.ChangeSpeakerDefaultState(sender.Enabled);
+        }
+
+        #endregion
+
+#region UiMethods
+
+        /// <summary>
+        /// Loading button images so we can switch them at runtime
+        /// </summary>
         private void LoadButtonImages()
         {
             speakerButtonOffImage = new UIImageView(View.Frame);
@@ -52,6 +120,10 @@ namespace Moduware.Tile.Speaker.iOS
             speakerButtonOnImage.Image = UIImage.FromBundle("SpeakerButtonOn");
         }
 
+        /// <summary>
+        /// Change state of main speaker button in UI
+        /// </summary>
+        /// <param name="active"></param>
         public void SetSpeakerButtonState(bool active)
         {
             _active = active;
@@ -65,43 +137,21 @@ namespace Moduware.Tile.Speaker.iOS
             }
         }
 
-        private void CoreReadyHandler(Object source, EventArgs e)
-        {
-            _speaker = new SpeakerTile(Core, this);
-        }
-
-        partial void SpeakerButtonDown(UIKit.UIButton sender)
-        {
-            SetSpeakerButtonState(!_active);
-            if(_active)
-            {
-                _speaker.TurnOn();
-            } else
-            {
-                _speaker.TurnOff();
-            }
-        }
-
-        partial void BackButtonDown(UIKit.UIButton sender)
-        {
-            Utilities.OpenDashboard();
-        }
-
-        partial void DefaultStateButtonChange(UISwitch sender)
-        {
-            _speaker.ChangeSpeakerDefaultState(sender.Enabled);
-        }
-
-        private void CoreConfigurationApplied(object sender, EventArgs e)
-        {
-            _speaker.RequestStatus();
-        }
-
+        /// <summary>
+        /// Change state of default speaker state switch
+        /// </summary>
+        /// <param name="active"></param>
         public void SetSpeakerDefaultState(bool active)
         {
             DefaultSwitch.Enabled = active;
         }
 
+        #endregion
+
+        /// <summary>
+        /// For moduware speaker we need establish secondary Bluetooth connection
+        /// </summary>
+        /// <param name="name">Bluetooth device name</param>
         public void PairToBluetoothDevice(string name)
         {
             var adapter = CrossBluetoothLE.Current.Adapter;
